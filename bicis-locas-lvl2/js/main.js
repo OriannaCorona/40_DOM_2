d8c4e2550e4f260f44218da2e1a668947d5dde6f
OriannaCorona <oriannalievanos12@gmail.com>
2016-10-19 09:03:21 -0500

    agrego el ejercicio terminado de bicis locas 2

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -4,23 +4,23 @@ function validateForm(){
 	var apellido = document.getElementById("lastname").value;
 	var mail = document.getElementById("input-email").value;
 	var clave = document.getElementById("input-password").value;
-	if(nombre == "" || apellido == "" || mail == "" || clave == ""){
-		confirm("Estos campos son OBLIGATORIOS");
-	}
-	camposSeleccion = document.getElementById("seleccion").selectedIndex;
-	if (camposSeleccion == null || camposSeleccion == 0){
-		return document.getElementById("selecBici").style.display = "inline";
-	}
 	if (!(/^[A-Z][a-zA-Z]*$/.test (nombre))){
 		return document.getElementById("nom").style.display = "inline";
 	}
 	if (!(/^[A-Z][a-zA-Z]*$/.test (apellido))){
 		return document.getElementById("ape").style.display = "inline";
 	}
+	if(mail == ""){
+		return document.getElementById("correo").style.display = "inline";
+	}
 	campoPassword = document.getElementById("input-password").value;
 	if (campoPassword < 6){
 		return document.getElementById("contra1").style.display = "inline";
 	}else if (campoPassword == "password" || campoPassword == "123456" || campoPassword == "098765"){
 		return document.getElementById("contra2").style.display = "inline";
+	}
+	camposSeleccion = document.getElementById("seleccion").selectedIndex;
+	if (camposSeleccion == null || camposSeleccion == 0){
+		return document.getElementById("selecBici").style.display = "inline";
 	}
 }
