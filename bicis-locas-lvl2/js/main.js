025ce86d93d13be095af18636fbcd8f80221963b
OriannaCorona <oriannalievanos12@gmail.com>
2016-10-19 15:09:54 -0500

    agrego el ejercicio mejorado y funcional

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,26 +1,47 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	/* Escribe tú código aquí*/ 
+	var spans = document.getElementsByClassName("falla");
+	while(spans.length>0){
+		/*spans i getNodeName*/ 
+	spans[0].parentElement.removeChild(spans[0]);
+	}
 	var nombre = document.getElementById("name").value;
 	var apellido = document.getElementById("lastname").value;
 	var mail = document.getElementById("input-email").value;
 	var clave = document.getElementById("input-password").value;
 	if (!(/^[A-Z][a-zA-Z]*$/.test (nombre))){
-		return document.getElementById("nom").style.display = "inline";
+		var validacion = document.createElement("span");
+		validacion.setAttribute("class","falla");
+		validacion.appendChild(document.createTextNode("Debe ingresar su nombre y debe comenzar con mayuscula")); 
+		document.getElementById("name").parentElement.appendChild(validacion);
 	}
 	if (!(/^[A-Z][a-zA-Z]*$/.test (apellido))){
-		return document.getElementById("ape").style.display = "inline";
+		var validacion = document.createElement("span");
+		validacion.setAttribute("class","falla");
+		validacion.appendChild(document.createTextNode("Debe ingresar su apellido y debe comenzar con mayuscula")); 
+		document.getElementById("lastname").parentElement.appendChild(validacion);
 	}
-	if(mail == ""){
-		return document.getElementById("correo").style.display = "inline";
+
+console.log(!(/\w+@+w+\.+[a-z]/.test(mail)));
+	if( mail==null || mail=="" || !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(mail))){
+		var validacion = document.createElement("span");
+		validacion.setAttribute("class","falla");
+		validacion.appendChild(document.createTextNode("Verifique su email")); 
+		document.getElementById("input-email").parentElement.appendChild(validacion);
 	}
 	campoPassword = document.getElementById("input-password").value;
-	if (campoPassword < 6){
-		return document.getElementById("contra1").style.display = "inline";
-	}else if (campoPassword == "password" || campoPassword == "123456" || campoPassword == "098765"){
-		return document.getElementById("contra2").style.display = "inline";
+	if (campoPassword < 6 || campoPassword == "password" || campoPassword == "123456" || campoPassword == "098765"){
+		var validacion = document.createElement("span");
+		validacion.setAttribute("class","falla");
+		validacion.appendChild(document.createTextNode("Verifique su contraseña")); 
+		document.getElementById("input-password").parentElement.appendChild(validacion);
 	}
 	camposSeleccion = document.getElementById("seleccion").selectedIndex;
 	if (camposSeleccion == null || camposSeleccion == 0){
-		return document.getElementById("selecBici").style.display = "inline";
+		var validacion = document.createElement("span");
+		validacion.setAttribute("class","falla");
+		validacion.appendChild(document.createTextNode("Seleccione algun tipo de bici")); 
+		document.getElementById("seleccion").parentElement.appendChild(validacion);
 	}
 }
+
